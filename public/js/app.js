c26b7c3989f84a12185ad25bfbdeaa845847e1ee
felixdumaslavoie <felixdumaslavoie@gmail.com>
2023-07-16 19:02:06 -0400

    Ajouts pour mise sur serveur

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,11 @@
 
 console.log("HELLO!")
 
-var ipAddress = "localhost"
+var ipAddress = "lakou-garou.duckdns.org"
 var port = 3000
 
 
-var socket = io.connect("http://localhost:3000"); 
+var socket = io.connect("https://" + ipAddress); 
 
 
 socketEvents(socket)
